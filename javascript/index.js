e6f88a9d29ffa5e7c072ad1d3388c651889620cc
aditya98899 <adiverma9889@gmail.com>
2025-01-06 13:52:22 +0530

    while do in js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,33 @@
-console.log
+console.log('Hello, World!');
+
+a = 10;
+
+console.log(a);
+
+msg = "Hello, Good Morning!";
+
+console.log(msg);
+
+console.log(typeof a);
+console.log(typeof msg);
+
+isHungry = null;
+console.log(isHungry);
+
+isHungry = true;
+console.log(typeof isHungry);
+
+console.log(1 == 1);
+console.log("1" == 1);
+console.log("1" === 1);
+
+console.log(2 ** 4);
+
+console.log(10 % 3);
+console.log(10 / 3);
+
+
+console.log(1+1);
+console.log("1"+1);
+console.log("1"+'1');
+console.log('My name is ' + msg + ' John Doe');
---
+++ javascript/loops.js
@@ -0,0 +1,72 @@
+// for (i = 0; i < 10; i++) {
+//     // console.log(i);
+
+// }
+
+// //wAP to print all even the number from 2 to 10
+
+// for (i = 1; i < 10; i++) {
+//     if (i % 2 === 0) {
+//         // console.log(i);
+//     }
+// }
+
+// //While loop
+
+// a = 10;
+
+// while (a < 20) {
+//     // console.log(a);
+
+//     a++;
+// }
+
+// //Do While Loop
+
+// b = 20
+// do {
+//     b++
+//     console.log(b);
+// }
+// while (b < 30)
+
+// a = 1;
+
+// while (a < 11) {
+//     console.log(a);
+
+//     a++;
+// }
+
+// a = 1
+
+// b = 0
+// do {
+//     b++
+//     console.log(b);
+// } while (b < 10)
+
+//odd numerb from for loop   
+for (i = 1; i < 10; i++) {
+    if (i % 2 === 1) {
+        console.log("odd",i);
+    }
+}
+// odd number using while
+
+a = 1;
+
+while (a < 10) {
+    if (a % 2 === 1)
+        console.log("ODD",a);
+
+    a++;
+}
+
+// odd number using do whille 
+b = 0
+do {
+    b++
+    if (b % 2 === 1)
+        console.log(b);
+} while (b < 10)
