ff44340dd25fe18177f1bd84a117b2c47d156e6d
aditya98899 <adiverma9889@gmail.com>
2025-01-21 14:10:17 +0530

    javascript basic

---
+++ javascript/index1.js
@@ -0,0 +1,20 @@
+console.log('Hello JavaScript');
+
+num = 56.34;
+console.log(num);
+console.log(typeof num);
+
+myname = "aditya";
+
+console.log(myname);
+
+//new airthemetic operator
+console.log(3 ** 4);
+console.log('1' = '1');
+console.log('1' = 1);
+console.log('1' = false);
+console.log(3 ** 4);
+console.log(3 ** 4);
+
+
+
